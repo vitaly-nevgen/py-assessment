77b55da65f3dec5c84ad41966bcb29a5c61882d6
Vitaly Nevgen <acidburn.by@gmail.com>
2015-11-04 22:25:48 +0300

    added stdout capture helper

---
+++ helpers/stdout_wrap.py
@@ -0,0 +1,19 @@
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+
+class CapturedOutput():
+
+    def __init__(self):
+        self.new_out, self.new_err = StringIO(), StringIO()
+        self.old_out, self.old_err = sys.stdout, sys.stderr
+
+        sys.stdout, sys.stderr = self.new_out, self.new_err
+
+    def getvalue(self):
+        output = self.new_out.getvalue().strip()
+        return output
+
+    def __del__(self):
+        sys.stdout, sys.stderr = self.old_out, self.old_err
